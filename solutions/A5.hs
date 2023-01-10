44d55e6eb4fb120040d0973caccb9d191132d6f1
Ed Eykholt <ed.eykholt@gmail.com>
2023-01-10 03:32:46 +0000

    Finished A5-2 Q#10

--- solutions/A5.hs
+++ solutions/A5.hs
@@ -59,17 +59,44 @@ play b p =
 -- *** Assignment 5-2 *** --
 
 -- Q#07
-
-printLogoDo = undefined
+printLogoDo :: IO()
+printLogoDo = do
+    logo <- readFile _LOGO_PATH_
+    putStrLn logo 
 
 -- Q#08
-
-firstPlayerDo = undefined
+firstPlayerDo :: IO Player
+firstPlayerDo = do
+    b <- _RANDOM_BOOL_ 
+    return $ getFirstPlayer b
 
 -- Q#09
-
-getMoveDo = undefined
+getMoveDo :: Board -> IO Move
+getMoveDo board = do
+    moveString <- getLine
+    validateMove board moveString
+    where 
+        validateMove :: Board -> String -> IO Move
+        validateMove b m = do
+            let move = stringToMove m
+            if isValidMove b move
+                then do
+                    return move
+                else do
+                    print "Invalid move! Try again"
+                    getMove b
 
 -- Q#10
-
-playDo = undefined
+playDo :: Board -> Player -> IO ()
+playDo b p = do
+    when _DISPLAY_LOGO_ printLogo
+    printBoard b
+    print (promptPlayer p)
+    move <- getMove b
+    let (gameState,newBoard) = playMove p b move
+    if gameState == XWon || gameState == OWon || gameState == Tie
+        then do
+            print (showGameState gameState)
+            printBoard newBoard
+        else
+            play newBoard $ switchPlayer p 
--- solutions/Main.hs
+++ solutions/Main.hs
@@ -7,7 +7,8 @@ import A4
 import A5
 
 main :: IO ()
-main = firstPlayer >>= play _EMPTY_BOARD_
+-- main = firstPlayer >>= play _EMPTY_BOARD_
+main = firstPlayerDo >>= playDo _EMPTY_BOARD_
 
     
 
